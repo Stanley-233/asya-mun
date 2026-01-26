82b40fcbcf260e82317e81a70b540029132eafee
Stanley <acc_stanley@foxmail.com>
2026-01-26 13:58:56 +0800

    fix: CORS issues

---
+++ src/main/kotlin/top/bearingwall/asya/config/CorsConfig.kt
@@ -0,0 +1,17 @@
+package top.bearingwall.asya.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
